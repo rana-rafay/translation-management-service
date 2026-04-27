ce0b2d1cb8c2d6708ef2c03b582d522461b679a8
Rana Rafay <ranarafayatwork@gmail.com>
2026-04-27 22:36:54 +0500

    Added Migrations & Models for Tags, Translations, and Pivot Table

---
+++ app/Models/Translation.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Translation extends Model
+{
+    use HasFactory;
+    protected $fillable = ['locale', 'key', 'value'];
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+}
---
+++ database/migrations/2026_04_27_172906_create_translation_tag_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('translation_tag', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('translation_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('tag_id')->constrained()->cascadeOnDelete();
+            $table->index('translation_id');
+            $table->index('tag_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('translation_tag');
+    }
+};
